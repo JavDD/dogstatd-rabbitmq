866838f6bd9b3cbbed84c0b68b786d69b19c879f
Dhruval-bhansali <121275212+Dhruval-bhansali@users.noreply.github.com>
2023-01-13 09:21:45 +0530

    Add extra condition to avoid script error
    Add extra condition to avoid script error if proper message is not received from rabbitmq

--- main.py
+++ main.py
@@ -12,34 +12,37 @@ def callback(ch, method, properties, body):
     
     message = body.decode('utf8').replace("'", '"')
     if message.count('measurement') == 1:
-        data = json.loads(message)
-        for b in data:
-            metric_name = b['measurement']
-            metric_value = b['fields']['value']
-            tags = b['tags']
-            tags_list=[]
-            for key,value in tags.items():
-                tags_list.append(f'{key}:{value}')
-            
-            # tags_value=tags_list.join(',')
-            statsd.gauge(metric_name,float(metric_value),tags=tags_list)
-            # print(metric_name,float(metric_value),type(tags))
+        if is_json(message):
+            data = json.loads(message)
+            for b in data:
+                metric_name = b['measurement']
+                metric_value = b['fields']['value']
+                tags = b['tags']
+                tags_list=[]
+                for key,value in tags.items():
+                    tags_list.append(f'{key}:{value}')
+                
+                # tags_value=tags_list.join(',')
+                statsd.gauge(metric_name,float(metric_value),tags=tags_list)
+                # print(metric_name,float(metric_value),type(tags))
     else:
         messages=message.replace(']','];')
         msgs = messages.split(';')
         for m in msgs:
             if '[{' in m:
-                data = json.loads(m)
-                for b in data:
-                    metric_name = b['measurement']
-                    metric_value = b['fields']['value']
-                    tags = b['tags']
-                    tags_list=[]
-                    for key,value in tags.items():
-                        tags_list.append(f'{key}:{value}')
+                if is_json(m):
+                    data = json.loads(m)
+                    for b in data:
+                        metric_name = b['measurement']
+                        metric_value = b['fields']['value']
+                        tags = b['tags']
+                        tags_list=[]
+                        for key,value in tags.items():
+                            tags_list.append(f'{key}:{value}')
 
-                    statsd.gauge(metric_name,float(metric_value),tags=tags_list)
-                    # print(metric_name,float(metric_value),type(tags))
+                        statsd.gauge(metric_name,float(metric_value),tags=tags_list)
+                        # print(metric_name,float(metric_value),type(tags))
+    
 
 
 options = {
@@ -60,3 +63,10 @@ channel.start_consuming()
 
 
 channel.stop_consuming()
+
+def is_json(myjson):
+  try:
+    json.loads(myjson)
+  except ValueError as e:
+    return False
+  return True
